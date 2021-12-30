e9d9a553de8b8b198684c892f79055f10040aa7e
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2021-12-30 14:14:29 +0100

    AES-GCM encryption implementation and tests

--- Noise/Noise.Core.Test/EncryptionTests.cs
+++ Noise/Noise.Core.Test/EncryptionTests.cs
@@ -9,7 +9,7 @@ namespace Noise.Core.Test
     public class EncryptionTests
     {
         [Fact]
-        public void HandlerShouldBeCreatedAndDeliverKeyPair()
+        public void AsymmetricHandlerShouldBeCreatedAndDeliverKeyPair()
         {
             var aeh = new AsymmetricEncryptionHandler();
 
@@ -21,13 +21,14 @@ namespace Noise.Core.Test
         }
 
         [Fact]
-        public void HandlerShouldEncryptAndDecryptUsingCorrectKeyPair()
+        public void AsymmetricHandlerShouldEncryptAndDecryptUsingCorrectKeyPair()
         {
             var firstAeh = new AsymmetricEncryptionHandler();
             string firstPrivateKey = firstAeh.GetPrivateKey();
             string firstPublicKey = firstAeh.GetPublicKey();
 
             string plainTextMessage = "Hello World!";
+            
             string cipher = AsymmetricEncryptionHandler.Encrypt(plainTextMessage, firstPublicKey);
 
             var xmlSerializer = new XmlSerializer(typeof(RSAParameters));
@@ -36,12 +37,12 @@ namespace Noise.Core.Test
 
             var secondAeh = new AsymmetricEncryptionHandler(rsaPrivateKeyParameter);
             string decryptedCipher = secondAeh.Decrypt(cipher);
-;
+
             Assert.Equal(plainTextMessage, decryptedCipher);
         }
 
         [Fact]
-        public void HandlerShoudEncryptButNotDecryptIncorrectKeyPair()
+        public void AsymmetricHandlerShoudEncryptButNotDecryptIncorrectKeyPair()
         {
             var firstAeh = new AsymmetricEncryptionHandler();
             string firstPublicKey = firstAeh.GetPublicKey();
@@ -51,6 +52,49 @@ namespace Noise.Core.Test
 
             var secondAeh = new AsymmetricEncryptionHandler();
             string decryptedCipher = secondAeh.Decrypt(cipher);
+
+            Assert.NotEqual(plainTextMessage, decryptedCipher);
+        }
+
+        [Fact]
+        public void SymmetricHandlerShouldBeCreatedAndEncryptData()
+        {
+            var seh = new SymmetricEncryptionHandler();
+
+            string plainTextMessage = "Hello World!";
+            var (cipher, key) = seh.Encrypt(plainTextMessage);
+
+            Assert.NotNull(cipher);
+            Assert.NotNull(key);
+        }
+
+        [Fact]
+        public void SymmetricHandlerShouldEncryptAndDecryptUsingCorrectKey()
+        {
+            var firstSeh = new SymmetricEncryptionHandler();
+
+            string plainTextMessage = "Hello World!";
+            var (cipher, key) = firstSeh.Encrypt(plainTextMessage);
+
+            int keyLength = key.Length;
+
+            var secondSeh = new SymmetricEncryptionHandler();
+            var decryptedCipher = secondSeh.Decrypt(cipher, key);
+
+            Assert.Equal(plainTextMessage, decryptedCipher);
+        }
+
+        [Fact]
+        public void SymmetricHandlerShouldEncryptButNotDecryptUsingTheIncorrectKey()
+        {
+            var firstSeh = new SymmetricEncryptionHandler();
+
+            string plainTextMessage = "Hello World!";
+            var (cipher, key) = firstSeh.Encrypt(plainTextMessage);
+
+            var secondSeh = new SymmetricEncryptionHandler();
+            var wrongKey = "This is not the correct key!";
+            var decryptedCipher = secondSeh.Decrypt(cipher, wrongKey);
 
             Assert.NotEqual(plainTextMessage, decryptedCipher);
         }
---
+++ Noise/Noise.Core/Encryption/SymmetricEncryptionHandler.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Buffers.Binary;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Noise.Core.Encryption
+{
+    public class SymmetricEncryptionHandler
+    {
+        private const int _keySize = 64;
+        private const int _saltSize = 8;
+        private const int _bytesDeriveIterations = 1000;
+
+        public SymmetricEncryptionHandler() { }
+
+        public (string cipher, string key) Encrypt(string data)
+        {
+            var keyBytes = new byte[_keySize];
+            
+            using var rngCrypto = new RNGCryptoServiceProvider();
+            rngCrypto.GetBytes(keyBytes);
+
+            var encodedKey = Convert.ToBase64String(keyBytes);
+
+            using var aesGcm = CreateAesGcm(encodedKey);
+
+            byte[] dataBytes = Encoding.Unicode.GetBytes(data);
+
+            int nonceSize = AesGcm.NonceByteSizes.MaxSize;
+            int tagSize = AesGcm.TagByteSizes.MaxSize;
+            int cipherSize = dataBytes.Length;
+
+            int encryptedDataLength = 4 + nonceSize + 4 + tagSize + cipherSize;
+
+            Span<byte> encryptedData = encryptedDataLength < 1024
+                ? stackalloc byte[encryptedDataLength]
+                : new byte[encryptedDataLength].AsSpan();
+
+            BinaryPrimitives.WriteInt32LittleEndian(encryptedData.Slice(0, 4), nonceSize);
+            BinaryPrimitives.WriteInt32LittleEndian(encryptedData.Slice(4 + nonceSize, 4), tagSize);
+
+            var nonce = encryptedData.Slice(4, nonceSize);
+            var tag = encryptedData.Slice(4 + nonceSize + 4, tagSize);
+            var cipherBytes = encryptedData.Slice(4 + nonceSize + 4 + tagSize, cipherSize);
+
+            RandomNumberGenerator.Fill(nonce);
+
+            aesGcm.Encrypt(nonce, dataBytes.AsSpan(), cipherBytes, tag);
+
+            var encodedCipher = Convert.ToBase64String(encryptedData);
+
+            return (encodedCipher, encodedKey);
+        }
+        
+        public string Decrypt(string cipher, string key)
+        {
+            Span<byte> encryptedData = Convert.FromBase64String(cipher).AsSpan();
+
+            using var aesGcm = CreateAesGcm(key);
+
+            int nonceSize = BinaryPrimitives.ReadInt32LittleEndian(encryptedData.Slice(0, 4));
+            int tagSize = BinaryPrimitives.ReadInt32LittleEndian(encryptedData.Slice(4 + nonceSize, 4));
+            int cipherSize = encryptedData.Length - 4 - nonceSize - 4 - tagSize;
+
+            var nonce = encryptedData.Slice(4, nonceSize);
+            var tag = encryptedData.Slice(4 + nonceSize + 4, tagSize);
+            var cipherBytes = encryptedData.Slice(4 + nonceSize + 4 + tagSize, cipherSize);
+
+            Span<byte> plainBytes = cipherSize < 1024
+                ? stackalloc byte[cipherSize]
+                : new byte[cipherSize];
+
+            try
+            {
+                aesGcm.Decrypt(nonce, cipherBytes, tag, plainBytes);
+
+                return Encoding.Unicode.GetString(plainBytes);
+            }
+            catch (CryptographicException)
+            {
+                return null;
+            }
+        }
+
+        private AesGcm CreateAesGcm(string key)
+        {
+            var salt = new byte[_saltSize];
+
+            byte[] keyBytes = new Rfc2898DeriveBytes(key, salt, _bytesDeriveIterations).GetBytes(16);
+            return new AesGcm(keyBytes);
+        }
+    }
+}
