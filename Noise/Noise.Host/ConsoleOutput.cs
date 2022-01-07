b5116b053aff0b6c0195c6fe04b77311b6897f5e
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2022-01-07 12:52:19 +0100

    Host output and process bolierplate

---
+++ Noise/Noise.Host/ConsoleOutput.cs
@@ -0,0 +1,39 @@
+﻿using Noise.Core.Abstraction;
+using Noise.Core.Extensions;
+using System;
+
+namespace Noise.Host
+{
+    public class ConsoleOutput : IOutput
+    {
+        public void WriteException(string message, Exception ex)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("[Error] {0} - {1}", message, ex.Message);
+            Console.ResetColor();
+        }
+
+        public void WriteLog(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("[Log] {0}", message);
+            Console.ResetColor();
+        }
+
+        public void WriteMessage(string senderPublicKey, string message, string senderIpAddress, string senderAlias)
+        {
+            string displayName = (senderAlias.IsEmpty()) ? senderPublicKey.Substring(0, 9) : senderAlias;
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("(@{0}): {1}//{2}", displayName, message, senderIpAddress);
+            Console.ResetColor();
+        }
+
+        public void WritePing(string senderIpAddress)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("[Ping] {0}", senderIpAddress);
+            Console.ResetColor();
+        }
+    }
+}
---
+++ Noise/Noise.Host/Program.cs
@@ -0,0 +1,83 @@
+﻿using Noise.Core.Abstraction;
+using Noise.Core.Client;
+using Noise.Core.File;
+using Noise.Core.Peer;
+using Noise.Core.Server;
+using Noise.Core.Services;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Noise.Host
+{
+    class Program
+    {
+        private const int SUCCESS = 0;
+        private const int FAILURE = 1;
+
+        private static PeerConfiguration _peerConfiguration;
+        private static IOutput _output;
+        private static IPacketService _packetService;
+
+        private static async Task<int> Main(string[] args)
+        {
+            try
+            {
+                // Singleton dependencies configuration
+                _peerConfiguration = await SetupPeer();
+                _output = new ConsoleOutput();
+                _packetService = new PacketService();
+
+                var ct = new CancellationToken(false);
+
+                // Server setup
+                using var server = new NoiseServer(_output, _packetService, _peerConfiguration);         
+                _ = Task.Run(async () => await server.StartAsync(ct));
+
+                // Client setup
+                using var client = new NoiseClient(_peerConfiguration);
+
+                while (!ct.IsCancellationRequested)
+                {
+                    Console.Write("> ");
+                    string inputCommand = Console.ReadLine();
+                    await ParseInput(inputCommand, client, _output, _peerConfiguration, ct);
+                }
+
+                return SUCCESS;
+            }
+            catch (Exception)
+            {
+                return FAILURE;
+            }
+        }
+
+        private async static Task<PeerConfiguration> SetupPeer()
+        {
+            try
+            {
+                if (FileHandler.PeerConfigurationFileExists())
+                {
+                    return FileHandler.GetPeerConfiguration();
+                }
+
+                var peerConfiguration = PeerConfiguration.Factory.Initialize();
+
+                await FileHandler.SavePeerConfigurationFile(peerConfiguration);
+
+                return peerConfiguration;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Fatal error. Can not utilize configuration.");
+                throw;
+            }
+        }
+
+        private async static Task ParseInput(string input, INoiseClient noiseClient, IOutput output, PeerConfiguration peerConfiguration, CancellationToken ct)
+        {
+            output.WriteLog(input);
+            throw new NotImplementedException();
+        }
+    }
+}
