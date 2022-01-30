f0658a0d0fea607524d045c835287df33e0f9316
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2022-01-30 19:07:24 +0100

    SHA1 handler implementation for packet checksum

---
+++ Noise/Noise.Core.Test/HashingTests.cs
@@ -0,0 +1,37 @@
+﻿using Noise.Core.Hashing;
+using Xunit;
+
+namespace Noise.Core.Test
+{
+    public class HashingTests
+    {
+        [Fact]
+        public void FastHandlerShouldHashSameInputSameOutput()
+        {
+            var plainData = "Hello World!";
+
+            var firstHash = FastHashingHandler.Hash(plainData);
+            var secondHash = FastHashingHandler.Hash(plainData);
+
+            Assert.NotNull(firstHash);
+            Assert.NotNull(secondHash);
+
+            Assert.Equal(firstHash, secondHash);
+        }
+
+        [Fact]
+        public void FastHandlerShouldHashDifferentInputDifferentOutput()
+        {
+            var firstPlainData = "Hello World!";
+            var secondPlainData = "Hello again!";
+
+            var firstHash = FastHashingHandler.Hash(firstPlainData);
+            var secondHash = FastHashingHandler.Hash(secondPlainData);
+
+            Assert.NotNull(firstHash);
+            Assert.NotNull(secondHash);
+
+            Assert.NotEqual(firstHash, secondHash);
+        }
+    }
+}
---
+++ Noise/Noise.Core/Hashing/FastHashingHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Noise.Core.Hashing
+{
+    public static class FastHashingHandler
+    {
+        public static string Hash(string plainData)
+        {
+            var dataHash = SHA1.HashData(Encoding.UTF8.GetBytes(plainData));
+
+            return Convert.ToBase64String(dataHash);
+        }
+    }
+}
