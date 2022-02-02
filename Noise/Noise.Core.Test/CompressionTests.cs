9adb136da137f9930d476c528280d9e018b808fe
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2022-02-02 16:25:27 +0100

    GZip compression handler and tests

---
+++ Noise/Noise.Core.Test/CompressionTests.cs
@@ -0,0 +1,22 @@
+﻿using Noise.Core.Compression;
+using System.Security.Cryptography;
+using Xunit;
+
+namespace Noise.Core.Test
+{
+    public class CompressionTests
+    {
+        [Fact]
+        public void DataShouldCompressAndDecompress()
+        {
+            var data = new byte[16384];
+            RandomNumberGenerator.Fill(data);
+
+            var compressedData = GZipCompressionHandler.Compress(data);
+
+            var decompressedData = GZipCompressionHandler.Decompress(compressedData);
+
+            Assert.Equal(data, decompressedData);
+        }
+    }
+}
---
+++ Noise/Noise.Core/Compression/GZipCompressionHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.IO.Compression;
+
+namespace Noise.Core.Compression
+{
+    public static class GZipCompressionHandler
+    {
+        public static byte[] Compress(byte[] dataBuffer)
+        {
+            using var compressedStream = new MemoryStream();
+            using var zipStream = new GZipStream(compressedStream, CompressionLevel.Optimal);
+            
+            zipStream.Write(dataBuffer, 0, dataBuffer.Length);
+            zipStream.Close();
+            
+            return compressedStream.ToArray();
+        }
+
+        public static byte[] Decompress(byte[] compressedDataBuffer)
+        {
+            using var compressedStream = new MemoryStream(compressedDataBuffer);
+            using var zipStream = new GZipStream(compressedStream, CompressionMode.Decompress);
+            using var resultStream = new MemoryStream();
+            
+            zipStream.CopyTo(resultStream);
+            
+            return resultStream.ToArray();
+        }
+    }
+}
