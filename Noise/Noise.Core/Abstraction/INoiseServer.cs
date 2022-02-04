227c0a1a5fa274179f22e405db297c142f71cfa8
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2022-02-04 15:01:39 +0100

    Removing the old implementation of the TCP server

--- Noise/Noise.Core/Abstraction/INoiseServer.cs
+++ Noise/Noise.Core/Abstraction/INoiseServer.cs
@@ -1,5 +1,4 @@
-﻿using Noise.Core.Events;
-using System;
+﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -7,11 +6,6 @@ namespace Noise.Core.Abstraction
 {
     public interface INoiseServer : IDisposable
     {
-        event EventHandler<ClientDisconnectedEventsArgs> OnClientDisconnected;
-        event EventHandler<PacketPairReceivedEventsArgs> OnMessageReceived;
-        event EventHandler<PacketReceivedEventsArgs> OnPingReceived;
-        event EventHandler<PacketPairReceivedEventsArgs> OnDiscoveryReceived;
-
         Task StartAsync(CancellationToken cancellationToken);
     }
 }
