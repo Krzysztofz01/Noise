943ed460929274933c0303a1d3ed57e48de6a33f
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2022-03-04 16:56:34 +0100

    Refactored output abstraction

--- Noise/Noise.Core/Abstraction/IOutput.cs
+++ Noise/Noise.Core/Abstraction/IOutput.cs
@@ -4,10 +4,15 @@ namespace Noise.Core.Abstraction
 {
     public interface IOutput
     {
+        [Obsolete]
         void WriteMessage(string senderPublicKey, string message, string senderIpAddress, string senderAlias);
+        [Obsolete]
         void WritePing(string senderIpAddress);
+        [Obsolete]
         void WriteException(string message, Exception ex);
+        [Obsolete]
         void WriteLog(string message);
+        [Obsolete]
         void WriteRaw(string value, bool newLine = true);
     }
 }
---
+++ Noise/Noise.Core/Abstraction/IOutputMonitor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Noise.Core.Abstraction
+{
+    public interface IOutputMonitor<TContext> : ILogger<TContext> where TContext : class
+    {
+        void WriteMessage(string senderPublicKey, string senderAlias, string senderEndpoint, string message);
+        void WritePing(string senderEndpoint);
+    }
+}
