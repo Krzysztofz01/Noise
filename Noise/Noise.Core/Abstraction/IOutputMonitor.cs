78b3f81de88e90a2b4e4f7de5513340aff4d9b62
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2022-03-07 17:17:18 +0100

    Monitor abstraction update

--- Noise/Noise.Core/Abstraction/IOutputMonitor.cs
+++ Noise/Noise.Core/Abstraction/IOutputMonitor.cs
@@ -1,9 +1,19 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using System;
 
 namespace Noise.Core.Abstraction
 {
-    public interface IOutputMonitor<TContext> : ILogger<TContext> where TContext : class
+    public interface IOutputMonitor<TContext> where TContext : class
     {
+        void LogError(string message);
+        void LogError(Exception exception);
+        void LogError(string message, Exception exception);
+
+        void LogWarning(string message);
+        void LogWarning(Exception exception);
+        void LogWarning(string message, Exception exception);
+
+        void LogInformation(string message);
+
         void WriteMessage(string senderPublicKey, string senderAlias, string senderEndpoint, string message);
         void WritePing(string senderEndpoint);
     }
