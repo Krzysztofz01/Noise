4b8b25d19a5009badef5606383e713a30ee23716
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2022-03-19 00:07:20 +0100

    SHA512 hashing handler

--- Noise/Noise.Core.Test/HashingTests.cs
+++ Noise/Noise.Core.Test/HashingTests.cs
@@ -69,5 +69,69 @@ namespace Noise.Core.Test
 
             Assert.NotEqual(firstHash, secondHash);
         }
+
+        [Fact]
+        public void LongHandlerShouldHashSameInputSameOutputToBytes()
+        {
+            var plainData = "Hello World!";
+            var plainDataBytes = Encoding.UTF8.GetBytes(plainData);
+
+            var firstHash = SHA512HashingHandler.HashToBytes(plainDataBytes);
+            var secondHash = SHA512HashingHandler.HashToBytes(plainDataBytes);
+
+            Assert.NotNull(firstHash);
+            Assert.NotNull(secondHash);
+
+            Assert.Equal(firstHash, secondHash);
+        }
+
+        [Fact]
+        public void LongHandlerShouldHashSameInputSameOutputToBase64()
+        {
+            var plainData = "Hello World!";
+            var plainDataBytes = Encoding.UTF8.GetBytes(plainData);
+
+            var firstHash = SHA512HashingHandler.HashToBase64(plainDataBytes);
+            var secondHash = SHA512HashingHandler.HashToBase64(plainDataBytes);
+
+            Assert.NotNull(firstHash);
+            Assert.NotNull(secondHash);
+
+            Assert.Equal(firstHash, secondHash);
+        }
+
+        [Fact]
+        public void LongHandlerShouldHashDifferentInputDifferentOutputToBytes()
+        {
+            var firstPlainData = "Hello World!";
+            var firstPlainDataBytes = Encoding.UTF8.GetBytes(firstPlainData);
+            var secondPlainData = "Hello again!";
+            var secondPlainDataBytes = Encoding.UTF8.GetBytes(secondPlainData);
+
+            var firstHash = SHA512HashingHandler.HashToBytes(firstPlainDataBytes);
+            var secondHash = SHA512HashingHandler.HashToBytes(secondPlainDataBytes);
+
+            Assert.NotNull(firstHash);
+            Assert.NotNull(secondHash);
+
+            Assert.NotEqual(firstHash, secondHash);
+        }
+
+        [Fact]
+        public void LongHandlerShouldHashDifferentInputDifferentOutputToBase64()
+        {
+            var firstPlainData = "Hello World!";
+            var firstPlainDataBytes = Encoding.UTF8.GetBytes(firstPlainData);
+            var secondPlainData = "Hello again!";
+            var secondPlainDataBytes = Encoding.UTF8.GetBytes(secondPlainData);
+
+            var firstHash = SHA512HashingHandler.HashToBase64(firstPlainDataBytes);
+            var secondHash = SHA512HashingHandler.HashToBase64(secondPlainDataBytes);
+
+            Assert.NotNull(firstHash);
+            Assert.NotNull(secondHash);
+
+            Assert.NotEqual(firstHash, secondHash);
+        }
     }
 }
---
+++ Noise/Noise.Core/Hashing/SHA512HashingHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Noise.Core.Hashing
+{
+    public static class SHA512HashingHandler
+    {
+        public static string HashToBase64(byte[] plainData)
+        {
+            var hashBytes = SHA512.HashData(plainData);
+
+            return Convert.ToBase64String(hashBytes);
+        }
+
+        public static byte[] HashToBytes(byte[] plainData)
+        {
+            return SHA512.HashData(plainData);
+        }
+    }
+}
