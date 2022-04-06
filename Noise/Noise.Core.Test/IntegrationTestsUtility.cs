1838cba60baf9d3b13b00970fdb0f7f07596a9db
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2022-04-06 23:23:00 +0200

    Noise server unit tests

--- Noise/Noise.Core.Test/IntegrationTestsUtility.cs
+++ Noise/Noise.Core.Test/IntegrationTestsUtility.cs
@@ -18,6 +18,7 @@ namespace Noise.Core.Test
 
     internal class MockupOutputMonitor : IOutputMonitor
     {
+        void IOutputMonitor.Clear() { }
         void IOutputMonitor.LogError(string message) { }
         void IOutputMonitor.LogError(Exception exception) { }
         void IOutputMonitor.LogError(string message, Exception exception) { }
@@ -27,9 +28,10 @@ namespace Noise.Core.Test
         void IOutputMonitor.LogWarning(string message, Exception exception) { }
         void IOutputMonitor.WriteIncomingMessage(string senderPublicKey, string senderAlias, string senderEndpoint, string message) { }
         void IOutputMonitor.WriteIncomingPing(string receiverEndpoint) { }
-        void IOutputMonitor.WriteIncomingSignature(string senderPublicKey) { }
+        void IOutputMonitor.WriteIncomingSignature(string senderPublicKey, string senderEndpoint) { }
         void IOutputMonitor.WriteOutgoingMessage(string message) { }
         void IOutputMonitor.WriteOutgoingPing(string senderEndpoint) { }
         void IOutputMonitor.WriteOutgoingSignature(string receiverPublicKey) { }
+        void IOutputMonitor.WriteRaw(string content, bool newLine) { }
     }
 }
