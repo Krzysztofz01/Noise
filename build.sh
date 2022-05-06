d3c7fc0ab7776274c36e58bfcc6f6d95c2e783f5
Krzysztof Zoń <krzysztof.zon2001@gmail.com>
2022-05-06 16:38:19 +0200

    Build location for patch script fix

--- build.sh
+++ build.sh
@@ -1,25 +1,20 @@
 #!/bin/bash
 
+# Noise peer host installation script
 set -e
 
+# Latest version build and binary preparation installation in temp directory
 dotnet publish ./Noise/Noise.Host/Noise.Host.csproj --runtime linux-x64 --output ./build --self-contained true --configuration Release /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true
 
 mkdir ./bin
-
 cp -r ./build/Noise.Host ./bin/noise
 
+# Removing usused files
 rm -rf build
-
 rm -rf Noise
-
 rm -rf resources
-
 rm -rf .git
-
 rm -f .gitignore
-
 rm -f build.cmd
-
 rm -f patch.cmd
-
 rm -f build.sh
--- patch.sh
+++ patch.sh
@@ -1,27 +1,40 @@
 #!/bin/bash
 
+# Noise peer host patching script
 set -e
 
+# Backup local binary and peer data
 mv ./bin ./bin-pre
 
+# Fetching the latest version
 mkdir ./patch
-
 git clone https://github.com/Krzysztofz01/Noise.git ./patch
 
-sudo chmod +x ./patch/build.sh
+# Latest version build and binary preparation installation in temp directory
+dotnet publish ./patch/Noise/Noise.Host/Noise.Host.csproj --runtime linux-x64 --output ./patch/build --self-contained true --configuration Release /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true
 
-sudo sh ./patch/build.sh
+mkdir ./patch/bin
+cp -r ./patch/build/Noise.Host ./patch/bin/noise
 
+# Removing usused files
+rm -rf ./patch/build
+rm -rf ./patch/Noise
+rm -rf ./patch/resources
+rm -rf ./patch/.git
+rm -f ./patch/.gitignore
+rm -f ./patch/build.cmd
+rm -f ./patch/patch.cmd
+rm -f ./patch/build.sh
+
+# Removing local version files
 rm -f LICENSE
-
 rm -f NOTICES
-
 rm -f README.md
-
 rm -f patch.sh
 
+# Replacing the latest version files and peer data transfer
 mv ./patch/* ./
-
 mv ./bin-pre/peer.noise ./bin/peer.noise
 
+# Installation cleanup
 rm ./bin-pre
