49d413ef32a4e225fdbf9c5f861f728885ca1b36
Krzysztof Zoń <krzysztof.zon2001@gmail.com>
2022-06-12 20:25:56 +0200

    Noise version dump for development branch

--- Noise/Noise.Core/Protocol/Constants.cs
+++ Noise/Noise.Core/Protocol/Constants.cs
@@ -4,7 +4,7 @@ namespace Noise.Core.Protocol
 {
     public static class Constants
     {
-        public const string Version = "v0.2.0-alpha";
+        public const string Version = "v0.3.0-alpha";
 
         public const Int32 ProtocolPort = 49490;
 
