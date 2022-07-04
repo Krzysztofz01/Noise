4c56b4ec3b0908c41f446bd18a2521cf97042671
Krzysztof Zoń <krzysztof.zon2001@gmail.com>
2022-07-04 18:17:35 +0200

    SendingTarget helper class implementation

---
+++ Noise/Noise.Host/SendingTarget.cs
@@ -0,0 +1,68 @@
+﻿using Noise.Core.Peer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Noise.Host
+{
+    internal class SendingTarget
+    {
+        private const int _publicKeyStripLength = 10;
+        private List<RemotePeer> _selectedPeers;
+
+        public void AddPeer(RemotePeer peer)
+        {
+            _selectedPeers.Add(peer);
+        }
+
+        public RemotePeer GetTarget()
+        {
+            if (!IsSelected() && IsGroup())
+                throw new InvalidOperationException("There must be a single peer selected to use this method.");
+
+            return _selectedPeers.Single();
+        }
+
+        public IEnumerable<RemotePeer> GetTargets()
+        {
+            if (!IsSelected() && !IsGroup())
+                throw new InvalidOperationException("There must be at least two peers selected to use this method.");
+
+            return _selectedPeers;
+        }
+
+        public bool IsGroup()
+        {
+            return _selectedPeers.Count > 1;
+        }
+
+        public bool IsSelected()
+        {
+            return _selectedPeers.Count > 0;
+        }
+
+        public string GetPrefix()
+        {
+            if (!IsSelected())
+                throw new InvalidOperationException("There must a at least one peer selected to use this method.");
+            
+            if (_selectedPeers.Count == 1)
+            {
+                var selectedPeer = GetTarget();
+
+                return selectedPeer.Alias != "Anonymous"
+                    ? selectedPeer.Alias
+                    : selectedPeer.PublicKey[.._publicKeyStripLength];
+            }
+
+            return $"Group({_selectedPeers.Count})";
+        }
+
+        private void Initialize()
+        {
+            _selectedPeers = new List<RemotePeer>();
+        }
+
+        public SendingTarget() => Initialize();
+    }
+}
