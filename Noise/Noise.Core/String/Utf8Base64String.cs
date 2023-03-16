e715774bd1a3b273a8752fcfbfdb320a961a4869
Krzysztof Zoń <krzysztof.zon2001@gmail.com>
2023-03-16 16:48:11 +0100

    Fea: Custom Strings implementation

---
+++ Noise/Noise.Core/String/Utf8Base64String.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+namespace Noise.Core.String
+{
+    public sealed class Utf8Base64String : IEnumerable, IEquatable<Utf8Base64String>, IComparable, IComparable<Utf8Base64String>
+    {
+        private readonly string _value;
+
+        private Utf8Base64String(string value) => _value = value;
+
+        public static Utf8Base64String FromString(string? value)
+        {
+            if (value is null) throw new ArgumentNullException(nameof(value));
+
+            var utf8EncodingBytes = Encoding.UTF8.GetBytes(value);
+            var base64Utf8EncodingString = Convert.ToBase64String(utf8EncodingBytes);
+
+            return new Utf8Base64String(base64Utf8EncodingString);
+        }
+
+        public static Utf8Base64String FromUtf8String(Utf8ValueString? value)
+        {
+            if (value is null) throw new ArgumentNullException(nameof(value));
+
+            return FromString(value.ToString());
+        }
+
+        public int Length => _value.Length;
+
+        public byte[] ToByteArray()
+        {
+            return Convert.FromBase64String(_value);
+        }
+
+        public override string ToString()
+        {
+            return _value;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(_value);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return Equals(obj as Utf8Base64String);
+        }
+
+        public bool Equals(Utf8Base64String? other)
+        {
+            if (other is null) return false;
+            return _value == other._value;
+        }
+
+        public int CompareTo(object? obj)
+        {
+            if (obj is null) return 1;
+            if (obj is not Utf8Base64String other)
+                throw new ArgumentException("Object is not an instance of the Utf8Base64String.");
+
+            return CompareTo(other);
+        }
+
+        public int CompareTo(Utf8Base64String? other)
+        {
+            if (other is null) return 1;
+            return _value.CompareTo(other._value);
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return _value.GetEnumerator();
+        }
+    }
+}
---
+++ Noise/Noise.Core/String/Utf8ValueString.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+namespace Noise.Core.String
+{
+    public sealed class Utf8ValueString : IEnumerable, IEquatable<Utf8ValueString>, IComparable, IComparable<Utf8ValueString>
+    {
+        private readonly string _value;
+
+        private Utf8ValueString(string value) => _value = value;
+
+        public static Utf8ValueString FromString(string? value)
+        {
+            if (value is null) throw new ArgumentNullException(nameof(value));
+
+            var defaultEncodingBytes = Encoding.Default.GetBytes(value);
+            var utf8EncodingString = Encoding.UTF8.GetString(defaultEncodingBytes);
+
+            return new Utf8ValueString(utf8EncodingString);
+        }
+
+        public static Utf8ValueString FromBase64String(Utf8Base64String? value)
+        {
+            if (value is null) throw new ArgumentNullException(nameof(value));
+
+            var decodedBase64ToUtf8 = Convert.FromBase64String(value.ToString());
+            var utf8EncodedString = Encoding.UTF8.GetString(decodedBase64ToUtf8);
+
+            return new Utf8ValueString(utf8EncodedString);
+        }
+
+        public int Length => _value.Length;
+
+        public byte[] ToByteArray()
+        {
+            return Encoding.UTF8.GetBytes(_value);
+        }
+
+        public override string ToString()
+        {
+            return _value;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(_value);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return Equals(obj as Utf8ValueString);
+        }
+
+        public bool Equals(Utf8ValueString? other)
+        {
+            if (other is null) return false;
+            return _value == other._value;
+        }
+
+        public int CompareTo(object? obj)
+        {
+            if (obj is null) return 1;
+            if (obj is not Utf8ValueString other)
+                throw new ArgumentException("Object is not an instance of the Utf8ValueString.");
+
+            return CompareTo(other);
+        }
+
+        public int CompareTo(Utf8ValueString? other)
+        {
+            if (other is null) return 1;
+            return _value.CompareTo(other._value);
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return _value.GetEnumerator();
+        }
+    }        
+}
