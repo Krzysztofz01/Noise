50f303ce44e1248ff47dec9a28fe552fd081a0b9
Krzysztof Zoń <krzysztof.zon2001@gmail.com>
2023-03-16 16:48:35 +0100

    Enh: Custom Strings unit tests implementation

---
+++ Noise/Noise.Core.Test/String/Utf8Base64StringTests.cs
@@ -0,0 +1,120 @@
+﻿using Noise.Core.String;
+using System;
+using System.Text;
+using Xunit;
+
+namespace Noise.Core.Test.String
+{
+    public class Utf8Base64StringTests
+    {
+        [Fact]
+        public void Should_Create_From_String()
+        {
+            var value = "Hello World!";
+
+            _ = Utf8Base64String.FromString(value);
+        }
+
+        [Fact]
+        public void Should_Create_From_Utf8Base64String()
+        {
+            var value = "Hello World!";
+            var utf8 = Utf8ValueString.FromString(value);
+
+            _ = Utf8Base64String.FromUtf8String(utf8);
+        }
+
+        [Fact]
+        public void Should_Tell_The_Value_Length()
+        {
+            var value = "Hello World!";
+            var base64 = Utf8Base64String.FromString(value);
+
+            var expected = Convert.ToBase64String(Encoding.UTF8.GetBytes(value)).Length;
+            var actual = base64.Length;
+
+            Assert.Equal(expected, actual);
+        }
+
+
+        [Fact]
+        public void Should_Return_Correct_Value_On_ToString_From_String()
+        {
+            var value = "Hello World!";
+            var base64 = Utf8Base64String.FromString(value);
+
+            var expectedToEqual = Convert.ToBase64String(Encoding.UTF8.GetBytes(value));
+            var expectedToNotEqual = Convert.ToBase64String(Encoding.UTF32.GetBytes(value));
+            var actual = base64.ToString();
+
+            Assert.Equal(expectedToEqual, actual);
+            Assert.NotEqual(expectedToNotEqual, actual);
+        }
+
+        [Fact]
+        public void Should_Return_Correct_Value_On_ToByteArray_From_String()
+        {
+            var value = "Hello World!";
+            var base64 = Utf8Base64String.FromString(value);
+
+            var expectedToEqual = Convert.FromBase64String(Convert.ToBase64String(Encoding.UTF8.GetBytes(value)));
+            var expectedToNotEqual = Convert.FromBase64String(Convert.ToBase64String(Encoding.UTF32.GetBytes(value)));
+            var actual = base64.ToByteArray();
+
+            Assert.Equal(expectedToEqual, actual);
+            Assert.NotEqual(expectedToNotEqual, actual);
+        }
+
+        [Fact]
+        public void Should_Return_Correct_Value_On_ToString_From_Utf8()
+        {
+            var value = "Hello World!";
+            var utf8 = Utf8ValueString.FromString(value);
+            var base64 = Utf8Base64String.FromUtf8String(utf8);
+
+            var expectedToEqual = Convert.ToBase64String(Encoding.UTF8.GetBytes(value));
+            var expectedToNotEqual = Convert.ToBase64String(Encoding.UTF32.GetBytes(value));
+            var actual = base64.ToString();
+
+            Assert.Equal(expectedToEqual, actual);
+            Assert.NotEqual(expectedToNotEqual, actual);
+        }
+
+        [Fact]
+        public void Should_Return_Correct_Value_On_ToByteArray_From_Base64()
+        {
+            var value = "Hello World!";
+            var utf8 = Utf8ValueString.FromString(value);
+            var base64 = Utf8Base64String.FromUtf8String(utf8);
+
+            var expectedToEqual = Convert.FromBase64String(Convert.ToBase64String(Encoding.UTF8.GetBytes(value)));
+            var expectedToNotEqual = Convert.FromBase64String(Convert.ToBase64String(Encoding.UTF32.GetBytes(value)));
+            var actual = base64.ToByteArray();
+
+            Assert.Equal(expectedToEqual, actual);
+            Assert.NotEqual(expectedToNotEqual, actual);
+        }
+
+        [Fact]
+        public void Should_Tell_If_Two_Instances_Are_Equal()
+        {
+            var value = "Hello World!";
+            var firstUtf8 = Utf8Base64String.FromString(value);
+            var secondUtf8 = Utf8Base64String.FromString(value);
+
+            Assert.Equal(firstUtf8, secondUtf8);
+        }
+
+        [Fact]
+        public void Should_Tell_If_Two_Instances_Are_Not_Equal()
+        {
+            var firstValue = "Hello World!";
+            var firstUtf8 = Utf8Base64String.FromString(firstValue);
+
+            var secondValue = "Hello other World!";
+            var secondUtf8 = Utf8Base64String.FromString(secondValue);
+
+            Assert.NotEqual(firstUtf8, secondUtf8);
+        }
+    }
+}
---
+++ Noise/Noise.Core.Test/String/Utf8ValueStringTests.cs
@@ -0,0 +1,119 @@
+﻿using Noise.Core.String;
+using System;
+using System.Text;
+using Xunit;
+
+namespace Noise.Core.Test.String
+{
+    public class Utf8ValueStringTests
+    {
+        [Fact]
+        public void Should_Create_From_String()
+        {
+            var value = "Hello World!";
+
+            _ = Utf8ValueString.FromString(value);
+        }
+
+        [Fact]
+        public void Should_Create_From_Utf8Base64String()
+        {
+            var value = "Hello World!";
+            var base64 = Utf8Base64String.FromString(value);
+
+            _ = Utf8ValueString.FromBase64String(base64);
+        }
+
+        [Fact]
+        public void Should_Tell_The_Value_Length()
+        {
+            var value = "Hello World!";
+            var utf8 = Utf8ValueString.FromString(value);
+
+            var expected = Encoding.UTF8.GetString(Encoding.Default.GetBytes(value)).Length;
+            var actual = utf8.Length;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Should_Return_Correct_Value_On_ToString_From_String()
+        {
+            var value = "Hello World!";
+            var utf8 = Utf8ValueString.FromString(value);
+
+            var expectedToEqual = Encoding.UTF8.GetString(Encoding.Default.GetBytes(value));
+            var expectedToNotEqual = Encoding.UTF32.GetString(Encoding.Default.GetBytes(value));
+            var actual = utf8.ToString();
+
+            Assert.Equal(expectedToEqual, actual);
+            Assert.NotEqual(expectedToNotEqual, actual);
+        }
+
+        [Fact]
+        public void Should_Return_Correct_Value_On_ToByteArray_From_String()
+        {
+            var value = "Hello World!";
+            var utf8 = Utf8ValueString.FromString(value);
+
+            var expectedToEqual = Encoding.UTF8.GetBytes(value);
+            var expectedToNotEqual = Encoding.UTF32.GetBytes(value);
+            var actual = utf8.ToByteArray();
+
+            Assert.Equal(expectedToEqual, actual);
+            Assert.NotEqual(expectedToNotEqual, actual);
+        }
+
+        [Fact]
+        public void Should_Return_Correct_Value_On_ToString_From_Base64()
+        {
+            var value = "Hello World!";
+            var base64 = Utf8Base64String.FromString(value);
+            var utf8 = Utf8ValueString.FromBase64String(base64);
+
+            var expectedToEqual = Encoding.UTF8.GetString(Encoding.Default.GetBytes(value));
+            var expectedToNotEqual = Encoding.UTF32.GetString(Encoding.Default.GetBytes(value));
+            var actual = utf8.ToString();
+
+            Assert.Equal(expectedToEqual, actual);
+            Assert.NotEqual(expectedToNotEqual, actual);
+        }
+
+        [Fact]
+        public void Should_Return_Correct_Value_On_ToByteArray_From_Base64()
+        {
+            var value = "Hello World!";
+            var base64 = Utf8Base64String.FromString(value);
+            var utf8 = Utf8ValueString.FromBase64String(base64);
+
+            var expectedToEqual = Encoding.UTF8.GetBytes(value);
+            var expectedToNotEqual = Encoding.UTF32.GetBytes(value);
+            var actual = utf8.ToByteArray();
+
+            Assert.Equal(expectedToEqual, actual);
+            Assert.NotEqual(expectedToNotEqual, actual);
+        }
+
+        [Fact]
+        public void Should_Tell_If_Two_Instances_Are_Equal()
+        {
+            var value = "Hello World!";
+            var firstUtf8 = Utf8ValueString.FromString(value);
+            var secondUtf8 = Utf8ValueString.FromString(value);
+
+            Assert.Equal(firstUtf8, secondUtf8);
+        }
+
+        [Fact]
+        public void Should_Tell_If_Two_Instances_Are_Not_Equal()
+        {
+            var firstValue = "Hello World!";
+            var firstUtf8 = Utf8ValueString.FromString(firstValue);
+
+            var secondValue = "Hello other World!";
+            var secondUtf8 = Utf8ValueString.FromString(secondValue);
+
+            Assert.NotEqual(firstUtf8, secondUtf8);
+        }
+    }
+}
